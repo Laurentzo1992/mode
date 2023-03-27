27059905581aa4c181a84a35a17f256a6f57b04a
Laurentzo1992 <lnikiema9@gmail.com.com>
2023-03-27 17:36:29 +0000

    deployement conf

--- lims/settings.py
+++ lims/settings.py
@@ -114,18 +114,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-#STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
+#MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "lims/static")
-#]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "lims/static")
+]
 
 MEDIA_URL = '/images/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, "lims/static/images")
+MEDIA_ROOT = os.path.join(BASE_DIR, "lims/static/images")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
