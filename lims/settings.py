a2fd3036f408ca610fd137fa71e80887cac5044a
Laurentzo1992 <lnikiema9@gmail.com.com>
2023-03-27 17:51:46 +0000

    deployement conf

--- lims/settings.py
+++ lims/settings.py
@@ -114,11 +114,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-#STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-#MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "lims/static")
