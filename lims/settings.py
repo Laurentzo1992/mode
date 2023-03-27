8ac8781c7e9008bed1c33bbb1e5156ed8d38c469
Laurentzo1992 <lnikiema9@gmail.com.com>
2023-03-27 15:43:33 +0000

    deployement conf

--- lims/settings.py
+++ lims/settings.py
@@ -1,21 +1,27 @@
 
 from pathlib import Path
 import os
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(env_file=str(BASE_DIR / "lims" / ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=k5lha5yrhrbx28+3gk0u($akvbbo2tlsc1yimf9)ch(s%!%7p"
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env.bool("DEBUG")
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+#SECRET_KEY = "django-insecure-=k5lha5yrhrbx28+3gk0u($akvbbo2tlsc1yimf9)ch(s%!%7p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -108,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
